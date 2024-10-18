c2444a9c394fd615008d207ab2e23a4b2ea3f852
gabriel-MF-11 <gabrielmoura170808@gmail.com>
2024-10-17 21:56:29 -0300

    meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("Olá");
+
+//git init --> inicializa o git
+//git remote add origin --> cria um entre o repositorio local e o repositorio no povedor de repositoris 
+//git add --> adiciona os arquivos no repositorio 
+//git status --> ele lista os arquivos (adicionados e modificados)
