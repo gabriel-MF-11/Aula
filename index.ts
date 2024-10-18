4b2ff815b8843311b1320f6dddaab582ca47ab36
gabriel-MF-11 <gabrielmoura170808@gmail.com>
2024-10-17 22:04:45 -0300

    segundo commit

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ console.log("Olá");
 //git remote add origin --> cria um entre o repositorio local e o repositorio no povedor de repositoris 
 //git add --> adiciona os arquivos no repositorio 
 //git status --> ele lista os arquivos (adicionados e modificados)
+
+console.log();
